3bcc6db02afc61e1277cae8709ac7674667945ec
hehaiyuan <hehaiyuan.hhy@gmail.com>
2020-03-26 17:06:08 +0800

    提交

--- template/temp.py
+++ template/temp.py
@@ -1,6 +1,20 @@
+# coding: utf-8
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution(object):
     def test(self):
-        return "";
+        return ""
 
 
 if __name__ == '__main__':
